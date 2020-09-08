f3b2013eb6bc13bfc0bd8256a83db177397a5f61
Annchous <57408376+annchous@users.noreply.github.com>
2020-09-08 22:18:12 +0300

    new

---
+++ IniParser/IniParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IniParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

aecade36806e9ffe0c580ac7432ee241bdc7b4db
Annchous <57408376+annchous@users.noreply.github.com>
2020-11-05 23:48:52 +0300

    fix: Main method now runs with command line arguments

--- OopLabs/BackupApp/Program.cs
+++ OopLabs/BackupApp/Program.cs
@@ -4,9 +4,7 @@ namespace BackupApp
 {
     class Program
     {
-        //static void Main(string[] args) => new ConsoleBackupApp(new[] { "n", "sep", "C:\\Users\\annchous\\Desktop\\testFile.txt", "C:\\Users\\annchous\\Desktop\\testFile2.txt", "|", "backups", "count", "3" }).Run();
-        static void Main(string[] args) => new ConsoleBackupApp(new[] { "n", "com", "C:\\Users\\annchous\\Desktop\\Backups", "C:\\Users\\annchous\\Desktop\\testFile.txt", "C:\\Users\\annchous\\Desktop\\testFile2.txt", "|", "backups", "count", "5" }).Run();
-        //static void Main(string[] args) => new ConsoleBackupApp(new[] { "e", "backups", "full" }).Run();
-        //static void Main(string[] args) => new ConsoleBackupApp(new[] { "e", "backups", "incremental" }).Run();
+        static void Main(string[] args) => new ConsoleBackupApp(args).Run();
+       
     }
 }
